fcb4c75944ee489f2da72b46bdb4df20b58b1bae
Alex <31609676+ASdzxcqweasdzxc@users.noreply.github.com>
2022-03-20 17:30:48 +0300

    Add files via upload

--- kapralov_04_2.asm
+++ kapralov_04_2.asm
@@ -1,56 +1,78 @@
 include console.inc
 
 .data
-x dw 128		;comment
-y dw 100		;comment
+x dw 32101		;for a
+y dw 25732		;for a
+yh db 0;		for b
+yl db 0;		for b
+z dd 0;			for b
+x1 dd 0;		for b
 
 .code
 start:
    
    ;got two words bx (bh-bl), cx (ch-cl) 
-   mov bx, x
-   mov cx, y
-   xor eax, eax
+   mov bx, x;				
+   mov cx, y;				
+   xor eax, eax;			clean eax
 
-   mov al, cl
-   mul bl
+   mov al, cl;				move cl (1st byte) part of number to al
+   mul bl;					Z0:= bl * cl
    ;outintln al,, '16?='
-   push eax
-   xor eax, eax
+   push eax;				move Z0 to stack
+   xor eax, eax;			clean eax
    ;got 1st result at stack
-   mov ah, ch
-   mul bl
-   push eax
-   xor eax, eax
+   mov al, ch;				move ch (2nd byte) part of number to ah
+   mul bl;					Z1:= bl * ch
+   shl eax, 8;				moved Z1 to correct place
+   push eax;				move Z1 to stack
+   xor eax, eax;			clean eax
    ;got 2nd result at stack
-   mov al, cl
-   mul bh
-   push eax
-   xor eax, eax
+   mov bl, bh;				move bh to lower part
+   mov al, cl;				move cl for multiplication
+   mul bl;					Z2:= bh * cl
+   shl eax, 8;				moved Z1 to correct place
+   push eax;				move Z2 to stack
+   xor eax, eax;			clean eax
    ;got 3rd result at stack
-   mov ah, ch
-   mul bh
-   push eax
-   xor eax, eax
+   mov al, ch
+   mul bl;					Z3:= bh * ch
    ;got 4th result at stack
+   shl eax, 16;				moved Z3 to correct place
+   xor ebx, ebx;			clean ebx
+
+   pop ebx;					take Z2 from stack (Z2:= bh * cl)
+   add eax, ebx;			Z:= Z3 + Z2
+
+   pop ebx;					take Z1 from stack (Z1:= bl * ch)
+   add eax, ebx;			Z:= Z + Z1
    
-   xor ebx, ebx
-   pop ebx
-   add eax, ebx
-   xor ebx, ebx
-   pop ebx
-   add eax, ebx
-   xor ebx, ebx
-   pop ebx
-   add eax, ebx
-   xor ebx, ebx
-   pop ebx
-   add eax, ebx
+   pop ebx;					take Z0 from stack (Z0:= bl * cl)
+   add eax, ebx;			Z:= Z + Z0
    
-   
-   outintln al,, 'al='
-   outintln ah,, 'ah='
+   outstrln 'a)'
    outintln eax,, 'eax='
+   
+   xor eax, eax;			prepare eax for division
+   
+   inintln z, 'Please enter positive double word: '
+   inintln yl, 'Please enter low part of Y (YL): '
+   inintln yh, 'Please enter high part of Y (YH): '
+   
+   mov ax, word ptr z;		move lower part from z to ax
+   mov dx, word ptr z + 2;	move higher part from z to dx
+   
+   mov bh, yh;				making word from bytes
+   mov bl, yl;				making word from bytes
+   ;outintln ax,, 'ax='
+   ;outintln dx,, 'ax='
+   ;outintln bx,, 'bx='
+   div bx
+   mov x1, eax
+
+   outstrln 'b)'
+   outintln bx,, 'Y='
+   outintln x1,, 'x1='
    
    pause 'press any button to continue'
    exit
