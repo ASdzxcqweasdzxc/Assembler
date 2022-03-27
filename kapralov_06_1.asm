7957c4f3a96695cd1403ee6f38ef27916e5746f6
Alex <31609676+ASdzxcqweasdzxc@users.noreply.github.com>
2022-03-27 12:15:21 +0300

    Add files via upload

--- kapralov_06_1.asm
+++ kapralov_06_1.asm
@@ -1,7 +1,7 @@
 include console.inc
 
 .data
-n dw 0		;number 0 < n < 2^16
+n dw 59049		;number 0 < n < 2^16
 k db -1		;power result
 x db 3		;working number
 
@@ -10,15 +10,13 @@ bad_number:
    outstrln 'bad number. try again'
 start:
    inintln n, 'Enter any positive x:'
-   mov eax,2;			
-   cmp n, ax
-   je bad_number
-   mov eax,0
-   cmp n, ax
-   jle bad_number
-
-   mov dx, 0;			prepare for div dx:ax
+   xor eax, eax
    mov ax, n;			move x value to ax
+   cmp ax, 1
+   jb bad_number
+
+   xor edx, edx;			prepare for div dx:ax
+   ;outintln eax
    movzx bx, x
    mov cl, k
 
@@ -27,6 +25,8 @@ main_div:
    div bx;				n_low(al):= n div 3, n_high(ah):= n mod 3
    cmp dx, 0;			if n_high = 0 then ZF:= 1
    jz main_div;			if n_high = 0 then goto main_div
+   cmp dx, 2;			in case delimiter = 2
+   jz main_div;			if n_high = 2 then goto main_div   
    cmp ax, 0;			if n_low = 0 then
    jz pech;				division done => goto pech
    
