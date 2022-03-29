168e970808fc603b822d4863c01915d8d9b9801e
Alex <31609676+ASdzxcqweasdzxc@users.noreply.github.com>
2022-03-29 22:40:55 +0300

    Add files via upload

--- kapralov_06_1.asm
+++ kapralov_06_1.asm
@@ -14,19 +14,21 @@ start:
    mov ax, n;			move x value to ax
    cmp ax, 1
    jb bad_number
+   mov cl, k
+   cmp ax, 2;			in case delimiter = 2
+   jz pech;				if n = 2 then goto pech   
 
    xor edx, edx;			prepare for div dx:ax
    ;outintln eax
    movzx bx, x
-   mov cl, k
 
 main_div:
    add cl, 1;			k:= k + 1
    div bx;				n_low(al):= n div 3, n_high(ah):= n mod 3
    cmp dx, 0;			if n_high = 0 then ZF:= 1
    jz main_div;			if n_high = 0 then goto main_div
-   cmp dx, 2;			in case delimiter = 2
-   jz main_div;			if n_high = 2 then goto main_div   
+   ;cmp dx, 2;			in case delimiter = 2
+   ;jz main_div;			if n_high = 2 then goto main_div   
    cmp ax, 0;			if n_low = 0 then
    jz pech;				division done => goto pech
    
